e2f1885a8408a4112f906c4218f0117c6e546c34
khoidauminh <casboi86@gmail.com>
2026-04-28 00:53:13 +0700

    Everything Axe: remove weakness altogether

--- src/main/java/dev/sillibeans/everythingpickaxe/items/EverythingAxeItem.java
+++ src/main/java/dev/sillibeans/everythingpickaxe/items/EverythingAxeItem.java
@@ -1,36 +1,7 @@
 package dev.sillibeans.everythingpickaxe.items;
 
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import org.jetbrains.annotations.NotNull;
-
 public class EverythingAxeItem extends EverythingTool {
-    private final static float ATTACK_SPEED = 0.8f;
-
     public EverythingAxeItem() {
-        super((float)Math.E, ATTACK_SPEED, 10, 3000);
-    }
-
-    @Override
-    public boolean hurtEnemy(ItemStack itemstack, @NotNull LivingEntity entity, @NotNull LivingEntity sourceentity) {
-
-        if (sourceentity instanceof Player player) {
-            final int cooldown = (int)(2.3f * 20f / ATTACK_SPEED);
-
-            player.addEffect(new MobEffectInstance(
-                MobEffects.WEAKNESS,
-                cooldown,
-                1,
-                false,
-                true
-            ));
-
-            player.getCooldowns().addCooldown(itemstack.getItem(), cooldown);
-        }
-
-        return super.hurtEnemy(itemstack, entity, sourceentity);
+        super((float)Math.E, 0.8f, 10, 3000);
     }
 }
