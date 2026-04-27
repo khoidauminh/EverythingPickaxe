d08463021bf2e9265b98c9f7045ed2c3f7912753
khoidauminh <casboi86@gmail.com>
2026-04-27 18:41:15 +0700

    1.9.2: Tweaks to the everything axe

--- src/main/java/dev/sillibeans/everythingpickaxe/items/EverythingAxeItem.java
+++ src/main/java/dev/sillibeans/everythingpickaxe/items/EverythingAxeItem.java
@@ -1,61 +1,34 @@
 package dev.sillibeans.everythingpickaxe.items;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 
 public class EverythingAxeItem extends EverythingTool {
-    private static final int DAMAGE = 9;
+    private final static float ATTACK_SPEED = 0.8f;
 
     public EverythingAxeItem() {
-        super((float)Math.E, 0.8f, DAMAGE, 3000);
+        super((float)Math.E, ATTACK_SPEED, 10, 3000);
     }
 
     @Override
-    public boolean mineBlock(
-            @NotNull ItemStack itemstack,
-            Level world,
-            @NotNull BlockState blockstate,
-            @NotNull BlockPos pos,
-            @NotNull LivingEntity entity
-    ) {
-        if (!world.isClientSide()) {
-            entity.addEffect(
-                new MobEffectInstance(
-                    MobEffects.WEAKNESS,
-                    100,
-                    999,
-                    false,
-                    false
-                )
-            );
-        }
+    public boolean hurtEnemy(ItemStack itemstack, @NotNull LivingEntity entity, @NotNull LivingEntity sourceentity) {
 
-        return super.mineBlock(itemstack, world, blockstate, pos, entity);
-    }
-
-    @Override
-    public boolean hurtEnemy(
-            ItemStack itemstack,
-            @NotNull LivingEntity entity,
-            @NotNull LivingEntity sourceentity
-    ) {
         if (sourceentity instanceof Player player) {
-            player.addEffect(
-                new MobEffectInstance(
-                    MobEffects.WEAKNESS,
-                    20*2,
-                    999,
-                    false,
-                    false
-                )
-            );
+            final int cooldown = (int)(2.3f * 20f / ATTACK_SPEED);
+
+            player.addEffect(new MobEffectInstance(
+                MobEffects.WEAKNESS,
+                cooldown,
+                1,
+                false,
+                true
+            ));
+
+            player.getCooldowns().addCooldown(itemstack.getItem(), cooldown);
         }
 
         return super.hurtEnemy(itemstack, entity, sourceentity);
