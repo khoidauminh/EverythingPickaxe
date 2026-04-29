a6838e7c0d5e1b051d626db9ef5795918a0da87b
khoidauminh <casboi86@gmail.com>
2026-04-29 20:29:03 +0700

    Adaptive handswing duration configuration

--- src/main/java/dev/sillibeans/everythingpickaxe/mixin/HandSwingMixin.java
+++ src/main/java/dev/sillibeans/everythingpickaxe/mixin/HandSwingMixin.java
@@ -1,7 +1,7 @@
 package dev.sillibeans.everythingpickaxe.mixin;
 
-import net.minecraft.world.entity.LivingEntity;
 import dev.sillibeans.everythingpickaxe.EverythingPickaxe;
+import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,10 +9,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public class HandSwingMixin {
-    @Inject(method = "getCurrentSwingDuration", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "getCurrentSwingDuration", at = @At("RETURN"), cancellable = true)
     public void swingTime(CallbackInfoReturnable<Integer> cir) {
         try {
-            cir.setReturnValue(EverythingPickaxe.CONFIG.swingTime.getAsInt());
-        } catch (Exception ignored) {}
+            final int currentVal = cir.getReturnValue();
+            final int newVal = currentVal * EverythingPickaxe.CONFIG.swingTime.getAsInt() / LivingEntity.SWING_DURATION;
+            cir.setReturnValue(newVal);
+        } catch (Exception ignored) {
+        }
     }
 }
